a52fe8699b6af79e1c1d2edb51ad33f6cf00f594
PruebasMenutech <84807153+PruebasMenutech@users.noreply.github.com>
2021-05-25 11:52:33 -0700

    Add files via upload

---
+++ docs/assets/js/zswipe.js
@@ -0,0 +1,30 @@
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+
+
+   var swiper = new Swiper('.swiper-container-promo', {
+      effect: 'flip',
+      grabCursor: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
